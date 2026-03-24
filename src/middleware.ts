6bc063e5c6a3e33481e1bc855cb4ce5d6d7bdf3d
Askebir Tadesse <askebirtadesse@gmail.com>
2026-03-25 00:37:09 +0300

    arcjet setup

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { notFound } from "next/navigation";
+import { forbidden, notFound } from "next/navigation";
+import arcjet, { detectBot, shield, slidingWindow } from "@arcjet/next";
+import { setUserCountryHeader } from "./lib/useCountryHeader";
+import { NextResponse } from "next/server";
+import { env } from "process";
 
 const isPublicRoute = createRouteMatcher([
   "/",
@@ -11,7 +15,31 @@ const isPublicRoute = createRouteMatcher([
 
 const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
 
+const aj = arcjet({
+  key: process.env.ARCJET_KEY!,
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE", "CATEGORY:MONITOR", "CATEGORY:PREVIEW"],
+    }),
+    slidingWindow({
+      mode: "LIVE",
+      interval: "1m",
+      max: 100,
+    }),
+  ],
+});
+
 export default clerkMiddleware(async (auth, req) => {
+  const decision = await aj.protect(
+    env.TEST_IP_ADDRESS
+      ? { ...req, ip: env.TEST_IP_ADDRESS, headers: req.headers }
+      : req,
+  );
+
+  if (decision.isDenied()) return forbidden();
+
   if (isAdminRoute(req)) {
     const user = await auth.protect();
     if (user.sessionClaims.role !== "admin") return notFound();
@@ -19,6 +47,14 @@ export default clerkMiddleware(async (auth, req) => {
 
   if (!isPublicRoute(req)) {
     await auth.protect();
+  }
+
+  if (!decision.ip.isVpn() && !decision.ip.isProxy()) {
+    const headers = new Headers(req.headers);
+    setUserCountryHeader(headers, decision.ip.country);
+    console.log(decision.ip.country);
+
+    return NextResponse.next({ request: { headers } });
   }
 });
 
