3b988f4312e87cdddbb4a0845c2dd99a9ed426b8
Askebir Tadesse <askebirtadesse@gmail.com>
2026-03-26 23:44:03 +0300

    single product page

--- src/app/(consumer)/products/[productId]/page.tsx
+++ src/app/(consumer)/products/[productId]/page.tsx
@@ -7,8 +7,11 @@ import {
 import { wherePublicCourseSections } from "@/src/features/courseSections/permissions/section";
 import { wherePublicLessons } from "@/src/features/lessons/permissions/lessons";
 import { wherePublicProducts } from "@/src/features/products/permissions/lessons";
+import { formatPrice } from "@/src/lib/formatters";
+import { sumArray } from "@/src/lib/sumArray";
 import { and, asc, eq } from "drizzle-orm";
 import { notFound } from "next/navigation";
+import { Suspense } from "react";
 
 export default async function ProductPage({
   params,
@@ -19,6 +22,29 @@ export default async function ProductPage({
   const product = await getPublicProduct(productId);
 
   if (product == null) return notFound();
+
+  const courseCount = product.course.length;
+  const lessonCount = sumArray(product.course, (course) =>
+    sumArray(course.courseSections, (s) => s.lessons.length),
+  );
+
+  return (
+    <div className="container my-6 ">
+      <div className="flex gap-16 items-center justify-between ">
+        <div className="flex gap-67 flex-col items-start ">
+          <div className="flex flex-col gap-2 ">
+            <Suspense
+              fallback={
+                <div className="text-xl">
+                  {formatPrice(product.priceInDollars)}
+                </div>
+              }
+            ></Suspense>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 async function getPublicProduct(id: string) {
