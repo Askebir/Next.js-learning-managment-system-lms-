950a9530a37f09fa0fffb0231810110ca640cf95
Askebir Tadesse <askebirtadesse@gmail.com>
2026-04-08 00:27:22 +0300

    canViewLesson purmision query function

--- src/features/lessons/permissions/lessons.ts
+++ src/features/lessons/permissions/lessons.ts
@@ -1,5 +1,14 @@
-import { LessonTable, UserRole } from "@/src/drizzle/schema";
-import { eq, or } from "drizzle-orm";
+import { db } from "@/src";
+import {
+  CourseSectionTable,
+  CourseTable,
+  LessonStatus,
+  LessonTable,
+  UserCourseAccessTable,
+  UserRole,
+} from "@/src/drizzle/schema";
+import { and, eq, or } from "drizzle-orm";
+import { wherePublicCourseSections } from "../../courseSections/permissions/section";
 
 export function canCreateLessons({ role }: { role?: UserRole }) {
   return role === "admin";
@@ -17,3 +26,44 @@ export const wherePublicLessons = or(
   eq(LessonTable.status, "public"),
   eq(LessonTable.status, "preview"),
 );
+
+export async function canViewLesson(
+  {
+    role,
+    userId,
+  }: {
+    userId: string | undefined;
+    role: UserRole | undefined;
+  },
+  lesson: { id: string; status: LessonStatus },
+) {
+  if (role === "admin" || lesson.status === "preview") return true;
+  if (userId == null || lesson.status === "private") return false;
+
+  const [data] = await db
+    .select({
+      courseId: CourseTable.id,
+    })
+    .from(UserCourseAccessTable)
+    .leftJoin(CourseTable, eq(CourseTable.id, UserCourseAccessTable.courseId))
+    .leftJoin(
+      CourseSectionTable,
+      and(
+        eq(CourseSectionTable.courseId, CourseTable.id),
+        wherePublicCourseSections,
+      ),
+    )
+    .leftJoin(
+      LessonTable,
+      and(eq(LessonTable.sectionId, CourseSectionTable.id), wherePublicLessons),
+    )
+    .where(
+      and(
+        eq(LessonTable.id, lesson.id),
+        eq(UserCourseAccessTable.userId, userId),
+      ),
+    )
+    .limit(1);
+
+  return data != null && data.courseId != null;
+}
